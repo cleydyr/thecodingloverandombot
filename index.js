301a089545fa65ee175cea491292285e47b3bbf3
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2017-03-30 16:54:28 -0300

    First working version 0.0.1

---
+++ index.js
@@ -0,0 +1,33 @@
+const codingLove = require('thecodinglove_parser/parser.js');
+const decode = require('decode-html');
+const TelegramBot = require('node-telegram-bot-api');
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+
+bot.onText(/\/love/, msg => {
+  codingLove.getRandomEntry((error, entry) => {
+    if (!!error) {
+      console.error(error);
+      bot.sendMessage(msg.chat.id, "Sorry. We have faced an internal error. Please try again.");
+    } else {
+      console.log(entry);
+      if (entry.image.endsWith(".gif")) {
+        bot.sendDocument(
+          msg.chat.id,
+          entry.image,
+          {
+            caption: decode(decode(entry.title))
+          });
+      }
+      else {
+        bot.sendPhoto(
+          msg.chat.id,
+          entry.image,
+          {
+            caption: decode(decode(entry.title))
+          });
+      }
+    }
+  });
+});
