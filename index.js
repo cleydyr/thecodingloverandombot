2ff4ee6195f71b48905352cac8465d7ae3bf7924
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2017-03-31 13:43:28 -0300

    Refactor to use Tumblr API wrapper instead of parsing HTML

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
-const codingLove = require('thecodinglove_parser/parser.js');
-const decode = require('decode-html');
-const TelegramBot = require('node-telegram-bot-api');
+'use strict';
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
-
-bot.onText(/\/love/, msg => {
-  console.log('Received /love');
-  
-  codingLove.getRandomEntry((error, entry) => {
-    if (!!error) {
-      console.error(error);
-      bot.sendMessage(msg.chat.id, "Sorry. We have faced an internal error. Please try again.");
-    } else {
-      console.log(entry);
-      if (entry.image.endsWith(".gif")) {
-        bot.sendDocument(
-          msg.chat.id,
-          entry.image,
-          {
-            caption: decode(decode(entry.title))
-          });
-      }
-      else {
-        bot.sendPhoto(
-          msg.chat.id,
-          entry.image,
-          {
-            caption: decode(decode(entry.title))
-          });
-      }
-    }
-  });
-});
+const bot = new (require('./lib/the-bot.js'))(process.env.TELEGRAM_TOKEN, process.env.TUMBLR_API_KEY);
 
 console.log('App started');
