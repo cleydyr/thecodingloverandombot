2b3ab10ea3f98bbdd24db63a617f4a14ad71f925
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-07-07 18:43:20 -0300

    Remove env variable as Tumblr is not used anymore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const bot = new (require('./lib/the-webhook-bot.js'))(process.env.TELEGRAM_TOKEN, process.env.TUMBLR_API_KEY);
+const bot = new (require('./lib/the-webhook-bot.js'))(process.env.TELEGRAM_TOKEN);
 
 console.log('App started');
